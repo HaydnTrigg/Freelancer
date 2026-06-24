f98b82936fdd900cc5d528043cd7cdc68c671244
Haydn Trigg <me@haydntrigg.com>
2026-06-24 21:23:11 +0930

    Update Alchemy.dll.cpp

---
+++ src/Alchemy.dll.cpp
@@ -0,0 +1,25 @@
+
+int dword_62578E4;
+int dword_62578E8;
+int dword_62578EC;
+int dword_62578F0;
+int dword_62578F4;
+int dword_62578F8;
+int dword_62578FC;
+int dword_6257900;
+int dword_6257904;
+
+extern "C" void sub_6201000()
+{
+    int zero = 0;
+
+    dword_62578E4 = zero;
+    dword_62578E8 = zero;
+    dword_62578EC = zero;
+    dword_62578F0 = zero;
+    dword_62578F4 = zero;
+    dword_62578F8 = zero;
+    dword_62578FC = zero;
+    dword_6257900 = zero;
+    dword_6257904 = zero;
+}
